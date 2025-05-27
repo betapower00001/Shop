bacbc187aba15241bdf269a64c4925afb964d62a
betapower00001 <betapower00001@gmail.com>
2025-05-27 17:00:01 +0700

    275568

--- src/app/payments/credit-card/page.tsx
+++ src/app/payments/credit-card/page.tsx
@@ -3,7 +3,9 @@
 import { Suspense } from 'react'
 import CreditCardClient from './CreditCardClient'
 
+// บังคับ dynamic rendering
 export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
 
 export default function CreditCardPage() {
   return (
