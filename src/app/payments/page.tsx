58d9e21daeff7b5cdb064c74c7143f72efa87623
betapower00001 <betapower00001@gmail.com>
2025-05-27 15:12:38 +0700

    27568

--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -1,9 +1,11 @@
  //src/app/payments/page.tsx
 
-
 import dynamic from 'next/dynamic';
 
-const PaymentsClient = dynamic(() => import('./PaymentClient'), { ssr: false });
+const PaymentsClient = dynamic(() => import('./PaymentClient'), { 
+  ssr: false,
+  loading: () => <p>Loading payments...</p>,
+});
 
 export default function PaymentsPage() {
   return <PaymentsClient />;
