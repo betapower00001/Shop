d35f1f0abb0cbf8175bc360ac325867ab0b1f8a9
betapower00001 <betapower00001@gmail.com>
2025-05-27 14:53:10 +0700

    27568

--- src/app/payments/PaymentClient.tsx
+++ src/app/payments/PaymentClient.tsx
@@ -1,10 +1,9 @@
-//src/app/payments/PaymentFormWrapper.tsx
 'use client'
 
 import { useSearchParams } from 'next/navigation'
 import PaymentForm from './PaymentForm'
 
-export default function PaymentFormWrapper() {
+export default function PaymentClient() {
   const searchParams = useSearchParams()
   const orderId = searchParams.get('orderId') || ''
 
--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -1,16 +1,14 @@
  //src/app/payments/page.tsx
 
-'use client'
-
 import { Suspense } from 'react'
-import PaymentFormWrapper from './PaymentFormWrapper'
+import PaymentClient from './PaymentClient'
 
 export const dynamic = 'force-dynamic'
 
 export default function PaymentPage() {
   return (
-    <Suspense fallback={<div>กำลังโหลด...</div>}>
-      <PaymentFormWrapper />
+    <Suspense fallback={<div>Loading...</div>}>
+      <PaymentClient />
     </Suspense>
   )
 }
